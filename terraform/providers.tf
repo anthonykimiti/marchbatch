d156c1ea788f91b6b025cf5ddf4c9e5b5aa4f63c
anthonykimiti <106841439+anthonykimiti@users.noreply.github.com>
2024-03-30 11:50:49 -0400

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,4 @@
-terraform {
+notterraform {
   required_version = ">=1.0"
 
   required_providers {
@@ -11,19 +11,12 @@ terraform {
       version = "~>3.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "test"
-    storage_account_name = "testabc12332"
-    container_name       = "statefile"
-    key                  = "dev.terraform.tfstate"
-    access_key = "8xc40u8Lim5jhhHDxp+2+scYRWSnvHth+kPH2B9EEy46CY0+Vy5yoaoekDiFnhNeycZfQxGGBKFb+ASt0jY/Vg=="
-  }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id   = "9112ff5f-1a9e-411d-b35f-4ec8bd46878a"
-  tenant_id         = "3f03adac-fb8a-4ad6-b7a7-f73cc0f6fd0d"
-  client_id         = "764fccda-d114-4bc9-9633-27a5258007d2"
-  client_secret     = "xXP8Q~Wd.hqn2WKcinIy6U66.DR9eK~TFl5dmbDo"
+  subscription_id   = "e04e5540-ac1f-4798-91ae-91b50b633bd9"
+  tenant_id         = "b5459be9-45cb-415a-a078-53817559250f"
+  client_id         = "0ba399ac-0b20-46ae-b7f6-5a0765d1f9cd"
+  client_secret     = "f.I8Q~JGjP~ctJ.6SYnDt1f5b-QoYJsv0e0tHcNu"
 }
